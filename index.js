b844e80659ecd2de6ce8262c4224d943d98ba0da
EkalavyanS <101187280+EkalavyanS@users.noreply.github.com>
2023-11-20 20:59:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import cors from "cors";
+import OpenAI from "openai";
+
+const port = 5000;
+const app = express();
+
+const openai = new OpenAI({
+  apiKey: "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",
+});
+
+app.use(
+  cors({
+    origin: "*", // Replace with your frontend's domain
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+  })
+);
+
+app.use(express.json());
+
+app.post("/imgUpload", async (req, res) => {
+  const image = req.query.img;
+  console.log("Getting into it")
+  console.log(image);
+  console.log(req.query);
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "A food item is shown in the image. Find the name of the food item in the image in one word",
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: image,
+            },
+          },
+        ],
+      },
+    ],
+  });
+  console.log("Completed")
+  console.log(response)
+
+  res.send(response.choices[0].message.content);
+});
+
+app.listen(port, () => console.log(`Server running on ${port}`));
---
+++ test.js
@@ -0,0 +1,28 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+ apiKey: "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",
+});
+
+
+async function main() {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          { type: "text", text: "What’s in this image?" },
+          {
+            type: "image_url",
+            image_url: {
+              "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            },
+          },
+        ],
+      },
+    ],
+  });
+  console.log(response.choices[0]);
+}
+main();
